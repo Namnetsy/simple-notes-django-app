effea810d5e19dbe2220b2202e354e8a314511b2
Namnetsy <namnetsy@gmail.com>
2020-10-05 20:40:00 +0300

    update Note model
    fix modified_at field being allowed to be blank but not accepting null

--- simple_notes/notes/models.py
+++ simple_notes/notes/models.py
@@ -17,7 +17,7 @@ class Note(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    modified_at = models.DateTimeField(blank=True)
+    modified_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.notebook.user.username}: {self.title}'
