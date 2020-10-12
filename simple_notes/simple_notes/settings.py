6c58d6cacbee321247f46cb96b91c1e6d74dca97
Namnetsy <namnetsy@gmail.com>
2020-10-12 09:23:37 +0300

    add django-environ dependency & .env.example file
    add environment variables for debug & secret key variables in settings.py

--- simple_notes/simple_notes/settings.py
+++ simple_notes/simple_notes/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 
 import os
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
